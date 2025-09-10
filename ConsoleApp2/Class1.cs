7fff10a6bae295007f3bf1a76ffb5bcfc81503c5
Илья Орлов ИТ-13 <ilyaorlovv6534@gmail.com>
2025-09-10 14:48:47 +0500

    Add files via upload

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class
+    {
+        public double fraction(double x)
+        {
+            int a = (int)x;
+            double b = x - a;
+            return b;
+        }
+
+        public int charToNum(char x)
+        {
+            int c = x - '0';
+            return c;
+        }
+
+        public bool is2Digits(int x)
+        {
+            int c=0;
+            while (x != 0)
+            {
+                x /= 10;
+                c++;
+            }
+            if (c == 2) return true;
+            else return false;
+        }
+
+        public bool isInRange(int a,int b,int num)
+        {
+            if (a < b)
+            {
+                if (a <= num && num <= b) return true;
+                else return false;
+            }
+            else
+            {
+                if (b <= num && num <= a) return true;
+                else return false;
+            }
+        }
+
+        public bool isEqual(int a,int b,int c)
+        {
+            if (a == b && a == c) return true;
+            else return false;
+        }
+
+        public int abs(int x)
+        {
+            if(x>0)
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using ConsoleApp2;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.1
+            //проверка на целое число
+            //Console.WriteLine("1.1");
+            //Class class11 = new Class();
+            //Console.Write("Введите вещественное число: ");
+            //double x = Convert.ToDouble(Console.ReadLine());
+            //double r = class11.fraction(x);
+            //Console.Write("Дробная часть числа: ");
+            //Console.WriteLine(r);
+
+            //1.3
+            //диапазон
+            //Console.WriteLine("1.3");
+            //Class class13 = new Class();
+            //Console.Write("Введите цифру: ");
+            //char c = Convert.ToChar(Console.ReadLine());
+            //double j = class13.charToNum(c);
+            //Console.WriteLine(j);
+
+            //1.5
+            //проверка на дробное число
+            //Console.WriteLine("1.5");
+            //Class class15 = new Class();
+            //Console.Write("Введите число: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //bool y = class15.is2Digits(n);
+            //Console.WriteLine(y);
+
+            //1.7
+            //проверка на целое число
+            //Console.WriteLine("1.7");
+            //Class class17 = new Class();
+            //Console.WriteLine("Введите диапазон: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите число: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //bool r = class17.isInRange(a,b,x);
+            //Console.WriteLine(r);
+
+            //1.9
+            Console.WriteLine("1.9");
+            Class class19 = new Class();
+            Console.WriteLine("Введите 3 числа: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int x = Convert.ToInt32(Console.ReadLine());
+            bool r = class19.isEqual(a, b, x);
+            Console.WriteLine(r);
+        }
+    }
+}
