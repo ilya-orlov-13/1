9ff329a6d4828176b8f8772322a3e6b845f96d2f
Илья Орлов ИТ-13 <ilyaorlovv6534@gmail.com>
2025-09-23 20:14:13 +0500

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
 using ConsoleApp2;
+using System;
+using System.Security.Claims;
 namespace ConsoleApp2
 {
     class Program
@@ -7,7 +8,6 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             //1.1
-            //проверка на целое число
             //Console.WriteLine("1.1");
             //Class class11 = new Class();
             //Console.Write("Введите вещественное число: ");
@@ -17,7 +17,6 @@ namespace ConsoleApp2
             //Console.WriteLine(r);
 
             //1.3
-            //диапазон
             //Console.WriteLine("1.3");
             //Class class13 = new Class();
             //Console.Write("Введите цифру: ");
@@ -26,7 +25,6 @@ namespace ConsoleApp2
             //Console.WriteLine(j);
 
             //1.5
-            //проверка на дробное число
             //Console.WriteLine("1.5");
             //Class class15 = new Class();
             //Console.Write("Введите число: ");
@@ -35,7 +33,6 @@ namespace ConsoleApp2
             //Console.WriteLine(y);
 
             //1.7
-            //проверка на целое число
             //Console.WriteLine("1.7");
             //Class class17 = new Class();
             //Console.WriteLine("Введите диапазон: ");
@@ -47,14 +44,192 @@ namespace ConsoleApp2
             //Console.WriteLine(r);
 
             //1.9
-            Console.WriteLine("1.9");
-            Class class19 = new Class();
-            Console.WriteLine("Введите 3 числа: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            int x = Convert.ToInt32(Console.ReadLine());
-            bool r = class19.isEqual(a, b, x);
-            Console.WriteLine(r);
+            //Console.WriteLine("1.9");
+            //Class class19 = new Class();
+            //Console.WriteLine("Введите 3 числа: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //bool r = class19.isEqual(a, b, x);
+            //Console.WriteLine(r);
+
+            //2.1
+            //Console.WriteLine("2.1");
+            //Class class21 = new Class();
+            //Console.WriteLine("Введите число: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int r = class21.abs(a);
+            //Console.WriteLine(r);
+
+            //2.3
+            //Console.WriteLine("2.3");
+            //Class class23 = new Class();
+            //Console.WriteLine("Введите число: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //bool r = class23.is35(a);
+            //Console.WriteLine(r);
+
+            //2.5
+            //Console.WriteLine("2.5");
+            //Class class25 = new Class();
+            //Console.WriteLine("Введите 3 числа: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //int r = class25.max3(a, b, c);
+            //Console.WriteLine(r);
+
+            //2.7
+            //Console.WriteLine("2.7");
+            //Class class27 = new Class();
+            //Console.WriteLine("Введите 2 числа: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int r = class27.sum2(a, b);
+            //Console.WriteLine(r);
+
+            //2.9
+            //Console.WriteLine("2.9");
+            //Class class29 = new Class();
+            //Console.Write("Введите день недели: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //string r = class29.day(a);
+            //Console.WriteLine(r);
+
+            //3.1
+            //Console.WriteLine("3.1");
+            //Class class31 = new Class();
+            //Console.Write("Введите цифру: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //string r=class31.listNums(x);
+            //Console.WriteLine(r);
+
+            //3.3
+            //Console.WriteLine("3.3");
+            //Class class33 = new Class();
+            //Console.Write("Введите цифру: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //string r = class33.chet(x);
+            //Console.WriteLine(r);
+
+            //3.5
+            //Console.WriteLine("3.5");
+            //Class class35 = new Class();
+            //Console.Write("Введите число: ");
+            //long a = Convert.ToInt32(Console.ReadLine());
+            //int r = class35.numLen(a);
+            //Console.WriteLine(r);
+
+            //3.7
+            //Console.WriteLine("3.7");
+            //Class class37 = new Class();
+            //Console.Write("Введите число: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //class37.square(a);
+
+            //3.9
+            //Console.WriteLine("3.9");
+            //Class class39 = new Class();
+            //Console.Write("Введите число: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //class39.rightTriangle(a);
+
+
+            //4.1
+            //Console.WriteLine("4.1");
+
+            //Class class41 = new Class();
+
+            //Console.Write("Введите количество элементов массива: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int[] arr = class41.GenerateRandomArray(n);
+
+            //class41.PrintArray(arr);
+
+            //Console.Write("Введите число: ");
+            //int x = int.Parse(Console.ReadLine());
+
+            //int result = class41.findFirst(arr, x);
+
+            //Console.WriteLine($"{result}");
+
+
+            //4.3
+            //Console.WriteLine("4.3");
+
+            //Class class43 = new Class();
+
+            //Console.Write("Введите количество элементов массива: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int[] arr = class43.GenerateRandomArray(n);
+
+            //class43.PrintArray(arr);
+
+            //int result = class43.maxAbs(arr);
+
+            //Console.WriteLine($"{result}");
+
+
+            //4.5
+            //Console.WriteLine("4.5");
+
+            //Class class45 = new Class();
+
+            //Console.Write("Введите количество элементов массива arr: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr = class45.GenerateRandomArray(n);
+            //class45.PrintArray(arr);
+
+            //Console.Write("Введите количество элементов массива ins: ");
+            //int n2 = int.Parse(Console.ReadLine());
+            //int[] ins = class45.GenerateRandomArray(n2);
+            //class45.PrintArray(ins);
+
+            //Console.Write("Введите позицию: ");
+            //int x = int.Parse(Console.ReadLine());
+
+            //int[] result = class45.add(arr,ins, x);
+
+            //class45.PrintArray(result);
+
+
+            //4.7
+            //Console.WriteLine("4.7");
+
+            //Class class47 = new Class();
+
+            //Console.Write("Введите количество элементов массива: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int[] arr = class47.GenerateRandomArray(n);
+
+            //class47.PrintArray(arr);
+
+            //int[] result = class47.reverseBack(arr);
+
+            //class47.PrintArray(result);
+
+
+            //4.9
+            //Console.WriteLine("4.9");
+
+            //Class class49 = new Class();
+
+            //Console.Write("Введите количество элементов массива: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int[] arr = class49.GenerateRandomArray(n);
+
+            //class49.PrintArray(arr);
+
+            //Console.Write("Введите число: ");
+            //int x = int.Parse(Console.ReadLine());
+
+            //int[] result = class49.findAll(arr,x);
+
+            //class49.PrintArray(result);
         }
     }
 }
